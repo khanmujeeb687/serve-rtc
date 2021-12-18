404ffe5659cf2ca1d185ced1a1a9b1d80cdbcfba
mujeeb <mujeeb.khan@zunroof.com>
2021-12-18 11:53:35 +0530

    fix

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,7 @@ module.exports = function(app, streams) {
 
   // GET streams as JSON
   var displayStreams = function(req, res) {
+    
     var streamList = streams.getStreams();
     // JSON exploit to clone streamList.public
     var data = (JSON.parse(JSON.stringify(streamList))); 
